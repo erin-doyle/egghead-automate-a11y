53763b1e277415925d8ce9ca33275eb2423dc321
Erin Doyle <erindoyle8080@gmail.com>
2021-01-24 14:04:40 -0500

    Added a unit test with jest-axe and react-testing-library

--- src/login/tests/Login.test.js
+++ src/login/tests/Login.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import Login from '../Login';
 
 expect.extend(toHaveNoViolations);
 
-it('should not violate accessibility rules', async () => {
-    const LoginPage = document.createElement('main');
+describe('Using ReactDOM', () => {
+    it('should not violate accessibility rules', async () => {
+        const LoginPage = document.createElement('main');
 
-    ReactDOM.render((
-        <Login history={{ push: jest.fn() }} />
-    ), LoginPage);
+        ReactDOM.render((
+            <Login history={{ push: jest.fn() }} />
+        ), LoginPage);
 
-    const results = await axe(LoginPage);
-    expect(results).toHaveNoViolations();
+        const results = await axe(LoginPage);
+        expect(results).toHaveNoViolations();
+    });
+});
+
+describe('Using react-testing-library', () => {
+    it('should not violate accessibility rules', async () => {
+        const { container } = render(
+            <Login history={{ push: jest.fn() }} />
+        );
+
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+    });
 });
