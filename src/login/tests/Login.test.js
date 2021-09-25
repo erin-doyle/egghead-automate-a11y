e9fd48ecb926aee99acc9b94d1af08cf784dd3ab
Erin Doyle <erin@theoryandprinciple.com>
2021-09-25 14:17:34 -0400

    Added a unit test with jest-axe and enzyme

--- src/login/tests/Login.test.js
+++ src/login/tests/Login.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
+import { mount } from 'enzyme';
+
 import Login from '../Login';
 
 expect.extend(toHaveNoViolations);
@@ -26,6 +28,17 @@ describe('Using react-testing-library', () => {
         );
 
         const results = await axe(container);
+        expect(results).toHaveNoViolations();
+    });
+});
+
+describe('Using enzyme', () => {
+    it('should not violate accessibility rules', async () => {
+        const LoginPage = mount(
+            <main><Login history={{ push: jest.fn() }} /></main>
+        );
+
+        const results = await axe(LoginPage.getDOMNode());
         expect(results).toHaveNoViolations();
     });
 });
